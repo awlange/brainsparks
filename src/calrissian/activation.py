bug fix with softmax

src/calrissian/activation.py
@@ -82,7 +82,11 @@ class Activation(object):
 
     @staticmethod
     def softmax(x):
-        return np.exp(x) / np.sum(np.exp(x))
+        ex = np.exp(x)
+        denoms = np.sum(ex, axis=1)
+        for i in range(len(ex)):
+            ex[i] /= denoms[i]
+        return ex
 
     @staticmethod
     def d_softmax(x):

src/calrissian/cost.py
@@ -57,13 +57,13 @@ class Cost(object):
 
     @staticmethod
     def categorical_cross_entropy(y, a):
-        return -np.sum(y * np.log(a))
+        return np.mean(-np.sum(y * np.log(a), axis=1))
 
     @staticmethod
     def d_categorical_cross_entropy(y, a, z):
         # z not used but here to maintain common interface
         # Note: only valid when used in combination with softmax activation in final layer
-        return a - y
+        return (a - y) / len(y)
 
     @staticmethod
     def binary_cross_entropy(y, a):

tests/cal.py
@@ -50,17 +50,19 @@ def main3():
 
 
 def fd():
-    net = Network(cost="mse")
+    net = Network(cost="categorical_cross_entropy")
     net.append(Dense(2, 5))
-    net.append(Dense(5, 3, activation="relu"))
+    net.append(Dense(5, 3, activation="softmax"))
 
     train_X = np.asarray([[0.2, -0.3]])
     train_Y = np.asarray([[0.0, 1.0, 0.0]])
 
-    # train_X = np.asarray([[0.2, -0.3], [0.6, -0.2], [0.8, 0.9], [0.1, 0.1]])
-    # train_Y = np.asarray([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
+    train_X = np.asarray([[0.2, -0.3], [0.6, -0.2], [0.8, 0.9], [0.1, 0.1]])
+    train_Y = np.asarray([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
 
     # Finite difference checking
+
+    net.cost(train_X, train_Y)
 
     db, dw = net.cost_gradient(train_X, train_Y)
 