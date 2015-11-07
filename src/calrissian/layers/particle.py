bug fix: restore bias gradient

src/calrissian/layers/particle.py
@@ -10,7 +10,7 @@ class ParticleInput(object):
         self.size = size
 
         # Positions
-        s = 2.0
+        s = 1.0
         self.r = np.random.uniform(-s, s, (size, 3))
 
 
@@ -29,11 +29,11 @@ class Particle(object):
         self.b = np.random.uniform(-s, s, (1, output_size))
 
         # Charges
-        s = 0.2
+        s = 0.1
         self.q = np.random.uniform(-s, s, output_size)
 
         # Positions
-        s = 2.0
+        s = 1.0
         self.r = np.random.uniform(-s, s, (output_size, 3))
 
     def feed_forward(self, a_in, r_in):

tests/particle_test.py
@@ -41,11 +41,11 @@ def main2():
 
 def fd():
 
-    train_X = np.asarray([[0.2, -0.3]])
-    train_Y = np.asarray([[0.0, 1.0, 0.0]])
-
     # train_X = np.asarray([[0.2, -0.3]])
     # train_Y = np.asarray([[0.0, 1.0, 0.0]])
+
+    train_X = np.asarray([[0.2, -0.3], [0.1, -0.9]])
+    train_Y = np.asarray([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
 
     net = ParticleNetwork(cost="categorical_cross_entropy", particle_input=ParticleInput(2),
                           regularizer=ParticleRegularize(0.25))