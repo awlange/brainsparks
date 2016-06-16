gradient bug fix for phase

src/calrissian/particle_network.py
@@ -167,10 +167,10 @@ class ParticleNetwork(object):
             d2 = dx**2 + dy**2 + dz**2
             exp_dij = np.exp(-layer.zeta * d2)
 
+            dt = None
             if layer.phase_enabled and prev_layer.phase_enabled:
                 dt = (prev_layer.theta - layer.theta[j]).reshape((prev_layer.output_size, 1))
                 exp_dij *= np.cos(dt)
-                exp_dij = 1.0/np.sqrt(d2)
 
             # Next delta
             next_delta += (qj * trans_delta_L_j) * exp_dij * trans_sigma_Z_l
@@ -230,10 +230,11 @@ class ParticleNetwork(object):
                 dz = (prev_layer.rz - layer.rz[j]).reshape((prev_layer.output_size, 1))
                 d2 = dx**2 + dy**2 + dz**2
                 exp_dij = np.exp(-layer.zeta * d2)
+
+                dt = None
                 if layer.phase_enabled and prev_layer.phase_enabled:
                     dt = (prev_layer.theta - layer.theta[j]).reshape((prev_layer.output_size, 1))
                     exp_dij *= np.cos(dt)
-                    exp_dij = 1.0/np.sqrt(d2)
 
                 # Next delta
                 next_delta += (qj * this_delta_j) * exp_dij * trans_sigma_Z_l

tests/particle_test.py
@@ -86,11 +86,13 @@ def fd():
     train_X = np.asarray([[0.2, -0.3], [0.1, -0.9]])
     train_Y = np.asarray([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
 
-    # net = ParticleNetwork(cost="categorical_cross_entropy", particle_input=ParticleInput(2))
-    net = ParticleNetwork(cost="categorical_cross_entropy", particle_input=ParticleInput(2), regularizer=ParticleRegularize(0.2))
-    net.append(Particle(2, 5, activation="leaky_relu"))
-    net.append(Particle(5, 4, activation="sigmoid"))
-    net.append(Particle(4, 3, activation="softmax"))
+    phase = True
+
+    net = ParticleNetwork(cost="categorical_cross_entropy", particle_input=ParticleInput(2, phase_enabled=phase))
+    # net = ParticleNetwork(cost="categorical_cross_entropy", particle_input=ParticleInput(2), regularizer=ParticleRegularize(0.2))
+    net.append(Particle(2, 5, activation="leaky_relu", phase_enabled=phase))
+    net.append(Particle(5, 4, activation="sigmoid", phase_enabled=phase))
+    net.append(Particle(4, 3, activation="softmax", phase_enabled=phase))
 
     # Finite difference checking
 
@@ -282,5 +284,5 @@ if __name__ == "__main__":
     # main()
     # main2()
     # main3()
-    main4()
-    # fd()
+    # main4()
+    fd()